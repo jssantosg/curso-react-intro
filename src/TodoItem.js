2cfafa1a0ffca31366cba89a1422dc7d16d82bfb
Johan Santos <jssantosg@gmail.com>
2025-01-21 00:55:40 -0800

    Se agregaron iconos con su respectiva funcionalidad

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,19 +1,28 @@
+import { CompleteIcon } from './CompleteIcon';
+import { DeleteIcon } from './DeleteIcon';
 import './TodoItem.css'
 
 function TodoItem(props) {
   return (
     <li className='TodoItem'>
-      <span
+      <CompleteIcon
+        completed={props.completed}
+        onComplete={props.onComplete}
+      />
+      {/* <span
         className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
         onClick={props.onComplete}
       >
           V
-      </span>
+      </span> */}
       <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>{props.text}</p>
-      <span className={`Icon Icon-delete`} onClick={props.onDelete}
+      <DeleteIcon
+        onDelete={props.onDelete}
+      />
+      {/* <span className={`Icon Icon-delete`} onClick={props.onDelete}
       >
         X
-      </span>
+      </span> */}
     </li>
   );
 }
